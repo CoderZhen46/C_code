90995df8d6b49a3d81e457a6f0a90a70d8e7623b
CoderZhen46 <1910490713@qq.com>
2021-08-16 16:36:23 +0800

    初始C语言（3）

---
+++ Project_1_14/Project_1_14/test.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	//int arr[10] = { 0 };
+//
+//	//arr[4] = 5;//[]就是下标引用操作符
+//	//2 + 3;
+//
+//	int ret = Add(2, 3);//（）函数调用操作符
+//
+//	return 0;
+//}
+
+
+//关键字
+
+//int main()
+//{
+//	int;
+//	char;
+//	return 0;
+//}
+
+//
+//void test(void)
+//{
+//
+//}
+//int main()
+//{
+//	//自动创建-销毁
+//	auto int a = 10;//局部变量-自动变量
+//	//auto可省略
+//	//signed int age;//默认有符号
+//	unsigned int age;
+//
+//	return 0;
+//}
+
+
+//typedef unsigned int uint;
+////小三 - 三三
+//
+//int main()
+//{
+//	unsigned int num1 = 10;
+//	uint num2 = 0;
+//
+//	return 0;
+//}
+
+
+//static - 静态的
+//static修饰局部变量
+//改变了局部变量的生命周期-作用域没有变化
+
+//static修饰全局变量
+//static修饰函数
+
+
+//void test()
+//{
+//	int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+
+//void test()
+//{
+//	static int a = 1;//static修饰局部变量
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
