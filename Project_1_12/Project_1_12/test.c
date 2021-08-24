08b21596b04894a3cbe30c3d0a4d281e6368cfc4
CoderZhen46 <1910490713@qq.com>
2021-08-24 23:04:59 +0800

    分支和循环语句（2）

--- Project_1_12/Project_1_12/test.c
+++ Project_1_12/Project_1_12/test.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+//≥ı ∂C”Ô—‘(2)
 #include <stdio.h>
 #include <string.h>
 //int num = 10;
---
+++ Project_1_19/Project_1_19/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	//在arr这个有序数组中查找一个数字k
+//	int k = 0;//7
+//	scanf("%d", &k);
+//	//查找
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (k == arr[i])
+//			printf("找到了，下标是: %d\n", i);
+//
+//	}
+//	return 0;
+//}
+
